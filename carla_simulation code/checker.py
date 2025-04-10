6250d6bb5241f5686d0727cb0e7b7547f284a27a
sankalp-rajeev <rajeevsankalp@gmail.com>
2025-04-10 00:45:49 -0400

    Removed unwanted files

--- carla_simulation code/checker.py
+++
@@ -1,5 +0,0 @@
-#Credits : https://github.com/vadim7s/SelfDrive/blob/master/RL_Full_Tutorial
-from stable_baselines3.common import env_checker
-from environment import CarEnv
-env = CarEnv()
-env_checker.check_env(env, warn=True, skip_render_check=True)
